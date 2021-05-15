198d7e7f27e06a8ef0176c7dcd0ec49b4512e61e
Keyner Baez <skerling19@hotmail.com>
2021-05-15 15:04:25 -0400

    added the services for get top rated movies

---
+++ src/services/getRatedMovies.js
@@ -0,0 +1,10 @@
+import {API_KEY, API_URL} from './settings'
+import { fromApiResponseToMovies } from 'services/utils';
+
+export default async function getNowPlaying() {
+  const apiURL = `${API_URL}/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`
+
+  return fetch(apiURL)
+    .then((res) => res.json())
+    .then(fromApiResponseToMovies)
+}
