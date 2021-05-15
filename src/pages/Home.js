186ae5805221752d71715e39dfddf0bf91cefdce
Keyner Baez <skerling19@hotmail.com>
2021-05-15 15:06:16 -0400

    added the page home with top rated movies

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState} from 'react';
 
 import Layout from 'components/Layout';
 import ListOfMovies from 'components/ListOfMovies';
 
-import { data } from 'data/data';
+import getTopRatedMovies from 'services/getRatedMovies';
 
 export default function Home() {
+    const [loading, setLoading] = useState(false);
+    const [moviesNow, setMoviesNow] = useState([]);
+
+    useEffect(() => {
+        setLoading(true);
+
+        getTopRatedMovies()
+            .then(movies => {
+                setMoviesNow(movies);
+                setLoading(false);
+            })
+            .catch(() => alert('Ocurrio algo'));
+    }, [])
+
+    if(loading) {
+        return (
+            <h1>Cargando...</h1>
+        )
+    }
+
     return (
         <>
             <Layout>
                 <div style={{ margin: "auto"}}>
-                    <ListOfMovies movies={data} title="Top Rated" />
+                    <ListOfMovies movies={moviesNow} title="Top Rated" />
                 </div>
             </Layout>
         </>
