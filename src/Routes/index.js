debe580692cd4bff3960cf54eff17523f349069b
Keyner Baez <skerling19@hotmail.com>
2021-05-15 18:59:15 -0400

    added the context provider for routes

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
 
+import { UserContextProvider } from 'context/UserContext';
+
 import Home from 'pages/Home';
 import NowPlaying from 'pages/NowPlaying';
 import DetailMovie from 'pages/DetailMovie';
@@ -8,12 +10,14 @@ import DetailMovie from 'pages/DetailMovie';
 export default function Routes() {
     return (
         <Router>
-            <Switch>
-                <Route exact path='/' component={Home}/>
-                <Route exact path='/now-playing' component={NowPlaying} />
-                <Route exact path='/movie/:id' component={DetailMovie} />
-                <Route path="*" component={<h1>Pagina no encontrada</h1>} />
-            </Switch>
+            <UserContextProvider>
+                <Switch>
+                    <Route exact path='/' component={Home}/>
+                    <Route exact path='/now-playing' component={NowPlaying} />
+                    <Route exact path='/movie/:id' component={DetailMovie} />
+                    {/* <Route path="*" component={<h1>Pagina no encontrada</h1>} /> */}
+                </Switch>
+            </UserContextProvider>
         </Router>
     )
 }
