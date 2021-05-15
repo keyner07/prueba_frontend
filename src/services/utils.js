45a3a4de1b4a97212816708a2732c13c33d827fa
Keyner Baez <skerling19@hotmail.com>
2021-05-15 15:03:15 -0400

    added the api key, api url and utils

---
+++ src/services/utils.js
@@ -0,0 +1,13 @@
+const fromApiResponseToMovies = apiResponse => {
+    const {results= []} = apiResponse;
+    if (Array.isArray(results)) {
+      const movies = results.map(movie => {
+        const {title, original_language, release_date, poster_path, vote_average, id} = movie;
+        return { title, id, original_language, release_date, poster_path, vote_average }
+      })
+      return movies
+    }
+    return []
+  }
+
+module.exports = { fromApiResponseToMovies }
