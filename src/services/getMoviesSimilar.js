355166b500402943b9a0365776aa496f43d8fac2
Keyner Baez <skerling19@hotmail.com>
2021-05-15 21:30:55 -0400

    added the services and the page for movies similar

---
+++ src/services/getMoviesSimilar.js
@@ -0,0 +1,10 @@
+import {API_KEY, API_URL} from './settings'
+import { fromApiResponseToMovies } from 'services/utils';
+
+export default async function getNowPlaying({ id }) {
+  const apiURL = `${API_URL}/movie/${id}/similar?api_key=${API_KEY}&language=en-US&page=1`
+
+  return fetch(apiURL)
+    .then((res) => res.json())
+    .then(fromApiResponseToMovies)
+}
