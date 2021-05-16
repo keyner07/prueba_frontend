b050967b3480a1479790c63e9ff2daaf503c1d0a
Keyner Baez <skerling19@hotmail.com>
2021-05-15 21:29:47 -0400

    change the uri for route

--- src/components/MovieItem/index.js
+++ src/components/MovieItem/index.js
@@ -7,7 +7,7 @@ import './styles.css';
 export default function MovieItem({ name, date, vote, image, id}) {
     return (
         <>
-            <Link to={`/movie/${id}`}>
+            <Link to={`/movies/${id}`}>
                 <Card className="Movie" bodyStyle={{ padding: 0 }}>
                         <img loading="lazy" alt={name} src={`https://image.tmdb.org/t/p/w500${image}`} />
                     <div className='custom-card'>
