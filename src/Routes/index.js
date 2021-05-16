7cc09f717758357a811f68e3b512b02dcb170e43
Keyner Baez <skerling19@hotmail.com>
2021-05-15 21:29:01 -0400

    added new routes

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -6,6 +6,7 @@ import { UserContextProvider } from 'context/UserContext';
 import Home from 'pages/Home';
 import NowPlaying from 'pages/NowPlaying';
 import DetailMovie from 'pages/DetailMovie';
+import MoviesSimilar from 'pages/MoviesSimilar'; 
 
 export default function Routes() {
     return (
@@ -14,7 +15,8 @@ export default function Routes() {
                 <Switch>
                     <Route exact path='/' component={Home}/>
                     <Route exact path='/now-playing' component={NowPlaying} />
-                    <Route exact path='/movie/:id' component={DetailMovie} />
+                    <Route exact path='/movies/:id' component={DetailMovie} />
+                    <Route exact path='/movies/:id/similar' component={MoviesSimilar} />
                     {/* <Route path="*" component={<h1>Pagina no encontrada</h1>} /> */}
                 </Switch>
             </UserContextProvider>
