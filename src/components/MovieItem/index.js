80237b1a2bd64f6f00f34cf66ebdd79566b3892c
Keyner Baez <skerling19@hotmail.com>
2021-05-15 23:40:47 -0400

    some changes to the component movie item

--- src/components/MovieItem/index.js
+++ src/components/MovieItem/index.js
@@ -2,20 +2,31 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Card } from 'antd';
 
+import Fav from 'components/Fav';
+
 import './styles.css';
 
 export default function MovieItem({ name, date, vote, image, id}) {
     return (
         <>
-            <Link to={`/movies/${id}`}>
-                <Card className="Movie" bodyStyle={{ padding: 0 }}>
-                        <img loading="lazy" alt={name} src={`https://image.tmdb.org/t/p/w500${image}`} />
+            <Card className="Movie" bodyStyle={{ padding: 0 }}>
+                <div className="Movie-buttons">
+                    <Fav
+                        id={id}
+                        name={name}
+                        date={date}
+                        vote={vote}
+                        image={image}
+                    ></Fav>
+                </div>
+                <Link to={`/movies/${id}`}>
+                    <img loading="lazy" alt={name} src={`https://image.tmdb.org/t/p/w500${image}`} />
                     <div className='custom-card'>
                         <h3>{name}</h3>
                         <p>{`Date: ${date} || Votes: ${vote}`}</p>
                     </div>
-                </Card>
-            </Link>
+                </Link>
+            </Card>
         </>
     )
 }
