3d923d35a13ac4af52782832bd1a62c449eafc7d
Keyner Baez <skerling19@hotmail.com>
2021-05-16 16:50:53 -0400

    change the name of the function

--- src/services/getMoviesSimilar.js
+++ src/services/getMoviesSimilar.js
@@ -1,7 +1,7 @@
 import {API_KEY, API_URL} from './settings'
 import { fromApiResponseToMovies } from 'services/utils';
 
-export default async function getNowPlaying({ id }) {
+export default async function getMovieSimilar({ id }) {
   const apiURL = `${API_URL}/movie/${id}/similar?api_key=${API_KEY}&language=en-US&page=1`
 
   return fetch(apiURL)
