c938025ce3d8f8aa23aa47f5eb556906c3032b76
Keyner Baez <skerling19@hotmail.com>
2021-05-16 16:42:03 -0400

    added the memo

--- src/components/MovieItem/index.js
+++ src/components/MovieItem/index.js
@@ -6,7 +6,7 @@ import Fav from 'components/Fav';
 
 import './styles.css';
 
-export default function MovieItem({ name, date, vote, image, id, rating}) {
+function MovieItem({ name, date, vote, image, id, rating}) {
     return (
         <>
             <Card className="Movie" bodyStyle={{ padding: 0 }}>
@@ -23,10 +23,14 @@ export default function MovieItem({ name, date, vote, image, id, rating}) {
                     <img loading="lazy" alt={name} src={`https://image.tmdb.org/t/p/w500${image}`} />
                     <div className='custom-card'>
                         <h3>{name}</h3>
-                        <p>{`Date: ${date} || Votes: ${vote} ${rating ? `Rate: ${rating}` : null }`}</p>
+                        <p>{`Date: ${date} || Votes: ${vote} ${rating ? `Rate: ${rating}` : '' }`}</p>
                     </div>
                 </Link>
             </Card>
         </>
     )
 }
+
+export default React.memo(MovieItem, (prevProps, nextProps) => {
+    return prevProps.id === nextProps.id;
+})
